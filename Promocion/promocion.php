61bf5e6cb6b5b083500e99961dc37a0a77a70339
wilsolis <millerapazasolis@gmail.com>
2020-10-09 11:38:55 -0500

    promocion2

--- Promocion/promocion.php
+++ Promocion/promocion.php
@@ -30,7 +30,21 @@
           <div class="card">
             <img src="img/combo1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
+              <h5 class="card-title">Pollo Asado al Horno</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer  text-center">
+            <a href="#" class="btn btn-primary">Ver mas</a>
+            </div>
+          </div>
+        </div>
+      </div><!--promocion-combo-->
+      <div class="promocion-combo">
+        <div class="card-deck">
+          <div class="card">
+            <img src="img/combo2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pastel de papa con carne</h5>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
             <div class="card-footer  text-center">
